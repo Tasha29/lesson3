cb78a41f12adb2f2960d18fcd751c58393eee0c0
boozar <boozarnovak@gmail.com>
2019-05-22 20:26:58 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			double x = 1, y = 0;
+
+
+			if (y == 0)
+			{
+				Console.WriteLine("Ошибка. Делитель не должен равняться нулю");
+			}
+			else
+			{
+				Console.WriteLine(x / y);
+			}
+
+			Console.WriteLine(y == 0 ? "Ошибка. Делитель не должен равняться нулю" : (x / y).ToString());
+
+
+		}
+
+	}
+}
